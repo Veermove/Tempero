bcea3fbbd33fb24134c3f74de830fa15345ecbdd
veermove <veermove@users.noreply.github.com>
2022-12-17 22:17:48 +0100

    Working on evaluating of expressions

--- src/eval.rs
+++ src/eval.rs
@@ -1,4 +1,4 @@
-use crate::{typedExpr::TExpression, lex::{Literal, Token, Operator}, parse::Expression};
+use crate::{lex::{Literal, Token, Operator}, parse::Expression};
 
 pub fn eval_exp(exp: Expression) -> Result<Literal, String> {
 
@@ -33,9 +33,126 @@ pub fn eval_exp(exp: Expression) -> Result<Literal, String> {
                 },
                 _ => unreachable!(),
             }
-            Expression::Binary() => unimplemented!(),
-            Expression::Conditional() => unimplemented!(),
-            Expression::Tuple() => unimplemented!(),
+            Expression::Binary(left, op, right) => match op {
+                Token::Operator(Operator::) =>
+            }
+            Expression::Conditional(pred, te, fe) => {
+                let pred = eval_exp(*pred)?;
+                if let Literal::Boolean(true) = pred {
+                    eval_exp(*te)
+                } else if let Literal::Boolean(false) = pred {
+                    eval_exp(*fe)
+                } else {
+                    unreachable!()
+                }
+            }
+            Expression::Tuple(_) => unimplemented!(),
         }
+    };
+    res
+}
+
+fn eval_eq(left: Expression, right: Expression) -> Result<Literal, String> {
+    Ok(Literal::Boolean(eval_exp(left)? == eval_exp(right)?))
+
+}
+
+fn eval_neq(left: Expression, right: Expression) -> Result<Literal, String> {
+    Ok(Literal::Boolean(eval_exp(left)? != eval_exp(right)?))
+}
+
+fn eval_greater(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_greatereq(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_lesser(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_lessereq(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_plus(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_minus(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_multip(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_div(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_not(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_and(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_or(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_bitand(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_bitor(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_shl(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_shr(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
+    }
+}
+
+fn eval_modulo(left: Expression, right: Expression) -> Result<Literal, String> {
+    match (eval_exp(left), eval_exp(right)) {
+
     }
 }
